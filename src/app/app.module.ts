cd8b93fe8d36ee0766820f8753ac5db128318c7b
Mike Vondrak <mvondrak@gmail.com>
2021-07-24 16:38:50 -0600

    initial firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAV7hD9iOJD_XMVsd8uU88g63ap-8_r7jI",
+  authDomain: "diveinnadmin.firebaseapp.com",
+  projectId: "diveinnadmin",
+  storageBucket: "diveinnadmin.appspot.com",
+  messagingSenderId: "100135443835",
+  appId: "1:100135443835:web:1ae5f4d17bda5ffa9631de",
+  measurementId: "G-W25N2VK7PZ"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
