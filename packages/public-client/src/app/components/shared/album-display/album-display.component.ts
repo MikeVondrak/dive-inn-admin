e083cf36b7893d0689dd41be23a73a15c9927512
rick haag <rick_haag@hotmail.com>
2022-02-10 22:26:34 -0700

    starting on sliders
    Co-authored-by: Mike <mvondrak@gmail.com>

--- packages/public-client/src/app/components/shared/album-display/album-display.component.ts
+++ packages/public-client/src/app/components/shared/album-display/album-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { AlbumDisplayImagePosition } from 'src/app/models/album-display.model';
 @Component({
   selector: 'app-album-display',
   templateUrl: './album-display.component.html',
@@ -11,9 +12,25 @@ export class AlbumDisplayComponent implements OnInit {
   @Input() description: string = '';
   @Input() imageUrls: string[] = []; 
 
+
+  /**
+   * Instead of using the image position array and getting into ElementRef and nativeElement
+   * use css classes for each position and remove the class to transition it into the frame
+   */
+  private positions: AlbumDisplayImagePosition[] = [
+    { x: 800, y: 0 },
+    { x: -800, y: 0 },
+    { x: 0, y: 600 },
+    { x: 0, y: -600 },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public getImagePosition(index: number): AlbumDisplayImagePosition {
+    const imgPosIdx = index % this.positions.length;
+    return this.positions[imgPosIdx];
+  }
 }
