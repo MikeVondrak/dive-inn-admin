960e3f2662c56b601f1f280ede9179f9ba7c0483
rick haag <rick_haag@hotmail.com>
2022-03-17 20:40:29 -0600

    env

--- packages/public-client/src/environments/environment.prod.ts
+++ packages/public-client/src/environments/environment.prod.ts
@@ -1,3 +1,26 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAV7hD9iOJD_XMVsd8uU88g63ap-8_r7jI",
+    authDomain: "diveinnadmin.firebaseapp.com",
+    projectId: "diveinnadmin",
+    storageBucket: "diveinnadmin.appspot.com",
+    messagingSenderId: "100135443835",
+    appId: "1:100135443835:web:1ae5f4d17bda5ffa9631de",
+    measurementId: "G-W25N2VK7PZ"
+  },
+  flickrConfig: {
+    user: '193893336@N02',
+    key: '306d51374865d4e6fa4b32b0fd5a7998',
+    secret: '65d13dd1fe465537'
+  },
+  hasuraConfig: {
+    gql_admin_secret: 'DiveInnHasAGqlSecret'
+  },
+  apolloConfig: {
+    APOLLO_KEY: 'service:Dive-Inn-Admin:TWTacm-GgHaEn2Ew4JQ8ng',
+    APOLLO_GRAPH_ID: 'Dive-Inn-Admin',
+    APOLLO_GRAPH_VARIANT: 'current',
+    APOLLO_SCHEMA_REPORTING: 'true',
+  }
 };
