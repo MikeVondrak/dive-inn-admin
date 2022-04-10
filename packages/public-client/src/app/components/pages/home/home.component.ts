b709f3c29a642aa9170b32d9e55b99d4623b9423
rick haag <rick_haag@hotmail.com>
2022-04-09 18:39:59 -0600

    design tweaks
    Co-authored-by: Mike <mvondrak@gmail.com>

--- packages/public-client/src/app/components/pages/home/home.component.ts
+++ packages/public-client/src/app/components/pages/home/home.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  public showLogo: boolean = false;
   public footerAddress: string = '1380 South Broadway';
   public footerPhone: string = '720-242-6157';
   public contactHours: string[] = ['M-W 10AM-1AM', 'TH-F 11AM-1AM', 'SA-SU 12PM-1AM'];
 
-  constructor() { }
+  // @HostListener('window:scroll', ['$event']) onScroll(event: Event) {
+  //   console.log('SCROLL EVENT', (event.target as Element).scrollTop);
+  // }
+  private destroy$ = new Subject<void>();
+
+
+  constructor() { 
+    fromEvent(window, 'scroll').pipe(takeUntil(this.destroy$)).subscribe((event: Event) => {
+      console.log('-----', document?.scrollingElement?.scrollTop + ' - ' + this.showLogo);
+      if (document?.scrollingElement?.scrollTop) {
+        if (document?.scrollingElement?.scrollTop > 250) {
+          this.showLogo = true;
+          console.log(this.showLogo);
+        } else {
+          this.showLogo = false;
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 }
